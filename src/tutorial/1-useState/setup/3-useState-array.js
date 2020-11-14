226a4cfd3e7ba27bc849f43be2103db90eefb1e4
Hutomo Sugianto <hutomo@premiro.com>
2020-11-14 18:51:18 +0700

    use state array

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+
+  const [people, setPeople] = useState(data)
+
+  const removeItem = (id) => {
+      const filteredPeople = people.filter((p) => {
+        return p.id !== id
+      })
+
+      setPeople(filteredPeople)
+  }
+
+  return (
+    <>
+      {people.map( ({id, name}) => {
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>
+              Remove Item
+            </button>
+          </div>
+        )
+      })}
+      <button className="btn" onClick={() => setPeople([])}>Remove All Items</button>
+    </>
+  )
 };
 
 export default UseStateArray;
