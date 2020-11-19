7b5c94498076850de80dee0ebc3a819cbb5aaeee
Hutomo Sugianto <hutomo@premiro.com>
2020-11-19 08:36:56 +0700

    use state cleanup

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,44 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+
+  const [value, setValue] = useState(window.innerWidth)
+
+  const checkSize = () => {
+    setValue(window.innerWidth)
+  }
+
+  // use effect yang berbeda bisa dipanggil beberapa kali
+  // dalam satu komponen
+  useEffect( () => {
+    console.log("useEffect")
+
+    // contoh yang salah:
+    // window.addEventListener('resize', setValue(window.innerWidth))
+
+    // contoh yang benar 1
+    // window.addEventListener('resize', checkSize)
+    
+    // contoh yang benar 2
+    window.addEventListener('resize', () => {
+      setValue(window.innerWidth)
+    })
+
+    // untuk mencegah memory leak
+    // ketika aplikasi semakin besar
+    // dibutuhkan cleanup function
+    return () => {
+      console.log("cleanup")
+      window.removeEventListener('resize', checkSize)
+    }
+  })
+
+  return (
+    <>
+      <h2>Window</h2>
+      <h4>{value} PX</h4>
+    </>
+  )
 };
 
 export default UseEffectCleanup;
