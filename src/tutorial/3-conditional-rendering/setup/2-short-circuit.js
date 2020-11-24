a4e4fb4ba11a119ab0ed034c4784f375e7f3bcb0
Hutomo Sugianto <hutomo@premiro.com>
2020-11-24 18:30:40 +0700

    short circuit & ternary

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,25 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState('')
+  const [isError, setIsError] = useState(false)
+
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  const toggleErrorBtn = () => {
+    setIsError(!isError)
+  }
+
+  return (
+    <>
+      <h1>{text || 'John Doe'}</h1>
+      <button className="btn" onClick={toggleErrorBtn}>Toggle Error</button>
+      {isError && <h1>Error...</h1>}
+
+      {isError ? <p>There is An Error</p> : <h2>There is No Error</h2>}
+    </>
+  )
 };
 
 export default ShortCircuit;
