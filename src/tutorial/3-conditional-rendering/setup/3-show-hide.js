0b2ed4e3144c739caf66a8a1b60bb6451be2a0ab
Hutomo Sugianto <hutomo@premiro.com>
2020-11-24 19:03:10 +0700

    Show Hide Function

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,42 @@
 import React, { useState, useEffect } from 'react';
 
+const Item = () => {
+
+  const [size, setSize] = useState(window.innerWidth)
+  
+  useEffect( () => {
+    window.addEventListener('resize', () => {
+      setSize(window.innerWidth)
+    })
+
+    // cleanup function
+    return () => {
+      window.removeEventListener('resize', () => {
+        setSize(window.innerWidth)
+      })
+    }
+  }, [])
+
+  return (
+    <div style={{ marginTop: '2rem' }}>
+      <h1>Window</h1>
+      <h2>Size: {size} px</h2>
+    </div>
+  )
+}
+
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+
+  const [show, setShow] = useState(false)
+
+  return (
+    <>
+      <button className="btn" onClick={() => setShow(!show)}>
+        Show / Hide
+      </button>
+      {show && <Item />}
+    </>
+  )
 };
 
 export default ShowHide;
