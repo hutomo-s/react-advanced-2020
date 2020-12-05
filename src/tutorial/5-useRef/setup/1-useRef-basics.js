52346afea2540efd590db73d78deb13f9cdf6c7f
Hutomo Sugianto <hutomo@premiro.com>
2020-12-05 16:39:02 +0700

    use ref basic

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,42 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react'
 
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
-};
 
-export default UseRefBasics;
+  const refFirstName = useRef(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    console.log(refFirstName.current.value)
+  }
+
+  // dont need dependency array
+  // because no use state which doing re render
+  useEffect( () => {
+    // focus on 1 dom element
+    refFirstName.current.focus() 
+  })
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="firstName">Name</label>
+            <input 
+              type="text" 
+              id="firstName" 
+              name="firstName" 
+              ref={refFirstName}
+            />
+          </div>
+          <button type="submit">Add Person</button>
+        </form>
+    </>
+  )
+}
+
+export default UseRefBasics
