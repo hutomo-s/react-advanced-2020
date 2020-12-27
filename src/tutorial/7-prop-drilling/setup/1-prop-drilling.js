18fc4e8752815d3c1f6747bf281d183a1445691a
Hutomo Sugianto <hutomo@premiro.com>
2020-12-27 10:05:02 +0700

    prop drilling

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,53 @@
 import React, { useState } from 'react';
+import {data} from '../../../data';
 
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [people, setPeople] = useState(data)
+  const removePerson = (id) => {
+    setPeople((people) => {
+      return people.filter((person) => person.id !== id)
+    })
+  }
+
+  return (
+    <section>
+      <h3>Prop Drilling</h3>
+      <List people={people} removePerson={removePerson} />
+    </section>
+  )
 };
+
+/**
+ * The Idea of Prop Drilling is following
+ * When List Component, technically does not need to have access
+ * to the removePerson() function 
+ * 
+ * But we have no other way to pass down our function
+ * into the SinglePerson
+ * unless we actually pass it to the List
+ * 
+ * Solution to Prop Drilling: Context API
+ */
+const List = ({people, removePerson}) => {
+  return (
+    <>
+      {people.map((person) => {
+        return <SinglePerson key={people.id} {...person} removePerson={removePerson} />
+      })}
+    </>
+  )
+}
+
+const SinglePerson = ({id, name, removePerson}) => {
+  return (
+    <div className="item">
+      <h4>{name}</h4>
+      <button onClick={() => removePerson(id)}>Remove</button>
+    </div>
+  )
+}
 
 export default PropDrilling;
