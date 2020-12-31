c2c6044d17deafc9f570bbd9a6a93f271dd0a93d
Wijoyo <wijoyo10@gmail.com>
2020-12-31 19:45:34 +0700

    context api

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -3,6 +3,14 @@ import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
+const PersonContext = React.createContext()
+
+// two components - Provider, Consumer
+// Provider works like a distributor
+
+// Prop Drilling starts when we pass the parameter
+// more than 1 level down
+
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
   const removePerson = (id) => {
@@ -11,14 +19,18 @@ const ContextAPI = () => {
     });
   };
   return (
-    <>
-      <h3>prop drilling</h3>
+    // wraps all your component tree
+    <PersonContext.Provider value={{removePerson, people}}>
+      <h3>Use Context</h3>
       <List people={people} removePerson={removePerson} />
-    </>
+    </PersonContext.Provider>
   );
 };
 
-const List = ({ people, removePerson }) => {
+const List = () => {
+
+  const {people} = useContext(PersonContext)
+
   return (
     <>
       {people.map((person) => {
@@ -26,7 +38,6 @@ const List = ({ people, removePerson }) => {
           <SinglePerson
             key={person.id}
             {...person}
-            removePerson={removePerson}
           />
         );
       })}
@@ -34,7 +45,10 @@ const List = ({ people, removePerson }) => {
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+
+  const {removePerson} = useContext(PersonContext)
+  
   return (
     <div className='item'>
       <h4>{name}</h4>
