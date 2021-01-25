7979058ac54c0b81357551145afafbd044489c02
Hutomo Sugianto <hutomo@premiro.com>
2021-01-25 16:55:57 +0700

    default value and default image

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+import defaultImage from './../../../assets/default-image.jpeg'
+
+const Product = ({image, name, price}) => {
+  // use short circuit operator
+  // will check image.url if image is defined
+  const imageUrl = image && image.url
+
+  return (
+    <article className='product'>
+      <img src={imageUrl || defaultImage} alt={name || 'Default Name'} />
+      <h4>{name || 'Default Name'}</h4>
+      <p>$ {price || '3.99'}</p>
+    </article>
+  );
 };
 
 export default Product;
